ba59c7baf5190558e5c5243265422bca90e1846f
Hungnth <thienhungnth@gmail.com>
2026-01-29 15:43:14 +0700

    fix: unify version variable naming for consistency in production and development environments

--- nth-notify.php
+++ nth-notify.php
@@ -25,12 +25,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 define( 'NTH_NOTIFY_IS_PRODUCTION', true );
 
 if ( ! NTH_NOTIFY_IS_PRODUCTION ) {
-	$assets_version = time();
+	$version = time();
 } else {
-	$assets_version = '1.1.0';
+	$version = '1.1.0';
 }
 
-define( 'NTH_NOTIFY_VERSION', $assets_version );
+define( 'NTH_NOTIFY_VERSION', $version );
 define( 'NTH_NOTIFY_PATH', plugin_dir_path( __FILE__ ) );
 define( 'NTH_NOTIFY_URL', plugin_dir_url( __FILE__ ) );
 define( 'NTH_NOTIFY_BASENAME', plugin_basename( __FILE__ ) );
